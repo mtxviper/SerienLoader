0966200e05e26df8396d27600427d69f2d897373
mtxviper <mtxviper@googlemail.com>
2013-02-23 18:03:14 +0100

    Added basic gui

--- SerienLoader/ViewModel/MainViewModel.cs
+++ SerienLoader/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,47 +11,67 @@ using SerienLoader.Utility;
 
 namespace SerienLoader.ViewModel
 {
-    class MainViewModel:INotifyPropertyChanged
-    {
-        private string _log;
+   internal class MainViewModel : INotifyPropertyChanged
+   {
+      private string _log;
 
+      public string Log
+      {
+         get { return _log; }
+         set
+         {
+            if (value == _log) return;
+            _log = value;
+            OnPropertyChanged("Log");
+         }
+      }
 
-        public MainViewModel()
-        {
-            Model= new MainModel();
-            ReadExistingEpisodesCommand = new RelayCommand(ReadExistingEpisodes);
-        }
+      private ObservableCollection<Show> _shows;
 
-        private void ReadExistingEpisodes(object obj)
-        {
-            Model.ReadExistingEpisodes(new List<string> {@"E:\Serien\",@"F:\Serien\"});
-            Log = Logger.LogString;
-        }
+      public ObservableCollection<Show> Shows
+      {
+         get { return _shows; }
+         set
+         {
+            if (Equals(value, _shows)) return;
+            _shows = value;
+            OnPropertyChanged("Shows");
+         }
+      }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+      public ICommand ReadExistingEpisodesCommand { get; set; }
+      public MainModel Model { get; set; }
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+      public MainViewModel()
+      {
+         Model = new MainModel();
+         Shows=new ObservableCollection<Show>();
+         ReadExistingEpisodesCommand = new RelayCommand(ReadExistingEpisodes);
+      }
 
-        public MainModel Model { get; set; }
+      private void ReadExistingEpisodes(object obj)
+      {
+         Model.ReadExistingEpisodes(new List<string> {@"E:\Serien\", @"F:\Serien\"});
+         Log = Logger.LogString;
+         foreach (KeyValuePair<string, Show> keyValuePair in Model.SerienFolderReader.Shows)
+         {
+            Shows.Add(keyValuePair.Value);
+         }
+      }
 
-        public string Log
-        {
-            get { return _log; }
-            set
-            {
-                if (value == _log) return;
-                _log = value;
-                OnPropertyChanged("Log");
-            }
-        }
+      
 
-        public ICommand ReadExistingEpisodesCommand { get; set; }
-    }
+      #region PropertyChanged implementation
 
-    
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged(string propertyName)
+      {
+         PropertyChangedEventHandler handler = PropertyChanged;
+         if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      #endregion
+   }
 }
